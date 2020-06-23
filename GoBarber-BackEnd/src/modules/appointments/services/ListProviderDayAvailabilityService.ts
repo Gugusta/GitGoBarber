ab920df3cc1d9c398ab89903e8466771048274e0
Gustavo Serao <gus1tavo@hotmail.com>
2020-06-23 16:36:30 -0300

    1.1.8 - Excluir horários antigos

--- GoBarber-BackEnd/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ GoBarber-BackEnd/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -45,15 +45,18 @@ class ListProviderDayAvailabilityService {
       { length: 10 },
       (_, index) => index + hourStart
     );
+    const currentDate = new Date(Date.now());
 
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHour = appointments.find(
         appointment => getHours(appointment.date) === hour
       );
 
+      const compareDate = new Date(year, month - 1, day, hour);
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
       };
     });
 
